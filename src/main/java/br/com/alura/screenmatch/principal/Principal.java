362914e287a81ceabfb5332e7761dbf302999906
JoaoDnz <jasd2407@gmail.com>
2026-03-17 22:47:16 -0300

    atualização temp.

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=2133fa55";
+
+    public void exibMenu(){
+        System.out.println("Digite o nome da série para a busca");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+
+    }
+}
